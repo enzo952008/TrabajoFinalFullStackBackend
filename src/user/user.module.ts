3a880628614f01c0fd6728fae1bd58f8bdeb42f8
enzo952008 <enzo952008@hotmail.com>
2024-09-09 21:40:51 -0300

    entidad user creada

---
+++ src/user/user.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { userProviders } from './user.provider';
+import { DatabaseModule } from 'src/database/database.module';
+
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [UserController],
+  providers: [
+    ...userProviders,
+    UserService],
+})
+export class UserModule {}
