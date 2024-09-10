dad3a223b492d11ff704e8d194f1cf65e8271e67
enzo952008 <enzo952008@hotmail.com>
2024-09-10 14:46:19 -0300

    entidad user funcionando

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,24 +1,25 @@
+
 import { Rol } from "src/rol/entities/rol.entity";
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity ('users')
+@Entity('users')
 export class User {
-    @PrimaryGeneratedColumn('uuid')
-    user_id: string;
+  @PrimaryGeneratedColumn('uuid')
+  user_id: string;
 
-    @Column({length:255})
-    name: string;
+  @Column({ length: 255 })
+  name: string;
 
-    @Column({length:255})
-    password: string;
+  @Column({ length: 255 })
+  password: string;
 
-    @Column ({length:255})
-    mail: string;
+  @Column({ length: 255 })
+  mail: string;
 
-    @Column()
-    photo_profile_url: string;
+  @Column({ nullable: true })  // Indica que la columna puede ser NULL en la base de datos
+  photo_profile_url: string;
 
-    @OneToOne(() => Rol, rol => rol.user)
-    @JoinColumn({ name: 'rol_id' }) // Define 'rol_id' como clave foránea en la tabla 'users'
-    rol: Rol;
+  @OneToOne(() => Rol, rol => rol.user)
+  @JoinColumn({ name: 'rol_id' })
+  rol: Rol;
 }
