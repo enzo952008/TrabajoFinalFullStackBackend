c5284bdc12332aeff601d64d1839219d86f156f5
julietazotes <julietaazotes@gmail.com>
2024-09-12 21:41:36 -0300

    Ajuste de usuario MySQL Juli

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -9,7 +9,7 @@ export const databaseProviders = [
         host: 'localhost',
         port: 3306,
         username: 'root',
-        password: 'enzo1995',
+        password: 'tgbrfv999X8',
         database: 'briosc',
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
