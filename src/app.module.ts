8bbd1f9a630045cd660fd24a118b4f639ba652db
enzo952008 <enzo952008@hotmail.com>
2024-09-23 20:00:47 -0300

    se implemento la entidad gastronomicplaces

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { RolModule } from './rol/rol.module'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { GastronomicPlaceModule } from './gastronomic-places/gastronomic-places.module';
 
 @Module({
-  imports: [RolModule, UserModule],
+  imports: [RolModule, UserModule, GastronomicPlaceModule],
   controllers: [],
   providers: [],
 })
---
+++ src/gastronomic-places/dto/create-gastronomic-place.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, IsString, Length, IsUrl } from 'class-validator';
+
+export class CreateGastronomicPlaceDto {
+  @IsString()
+  @Length(1, 255, { message: 'El nombre debe tener entre 1 y 255 caracteres' })
+  @IsNotEmpty({ message: 'El nombre es obligatorio' })
+  name: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'La descripción es obligatoria' })
+  description: string;
+
+  @IsUrl({}, { message: 'La URL de la imagen debe ser válida' })
+  @IsNotEmpty({ message: 'La URL de la imagen es obligatoria' })
+  image_url: string;
+}
+
