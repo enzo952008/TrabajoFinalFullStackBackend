84b07fd768cf8b7d450a509914038cc5c46de346
julietazotes <julietaazotes@gmail.com>
2024-10-17 20:44:31 -0300

    agregado de validaciones con el decorador @IsStrongPassword en el dto de user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, IsEmail, IsOptional } from 'class-validator';
+import { IsString, IsNotEmpty, IsEmail, IsOptional, isStrongPassword, IsStrongPassword } from 'class-validator';
 
 export class CreateUserDto {
     @IsString()
@@ -7,6 +7,19 @@ export class CreateUserDto {
 
     @IsString()
     @IsNotEmpty()
+    @IsStrongPassword(
+        {
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+        },
+        {
+            message:
+            "La contraseña debe contener un mínimo de 8 caracteres, y al menos 1 mayúscula, 1 minúscula, 1 número y un carácter especial",
+        }
+    )
     readonly password: string;
 
     @IsEmail()
