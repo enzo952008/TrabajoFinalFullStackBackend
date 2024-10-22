b61f0838c37918fde8ae147234cef1c92115c17a
enzo952008 <enzo952008@hotmail.com>
2024-10-22 18:15:28 -0300

    encriptado de contrasena

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -7,21 +7,17 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   user_id: string;
 
-  @Column({ length: 255 })
+  @Column({type: 'varchar', length: 255 })
   name: string;
 
-  @Column({ length: 255 })
+  @Column({type: 'varchar', length: 255 })
   password: string;
 
-  @Column({ length: 255 })
+  @Column({type: 'varchar', length: 255, unique: true })
   mail: string;
 
   @Column({ nullable: true })  // Indica que la columna puede ser NULL en la base de datos
   photo_profile_url: string;
-
-  // @OneToOne(() => Rol, rol => rol.user)
-  // @JoinColumn({ name: 'rol_id' })
-  // rol: Rol;
 
   @ManyToOne(type => Rol, rol => rol.user)
   @JoinColumn({ name: 'rol_id' })
