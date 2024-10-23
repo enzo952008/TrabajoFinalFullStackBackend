d469eae94fac5f256e0a708ee4ec2186a4290d9e
julietazotes <julietaazotes@gmail.com>
2024-10-23 17:40:16 -0300

    Implementación de medidas de seguridad contra ataques frecuentes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,28 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { GastronomicPlaceModule } from './gastronomic-places/gastronomic-places.module';
 import { LodgingModule } from './lodging/lodging.module';
-
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [RolModule, UserModule, LodgingModule, GastronomicPlaceModule],
+  imports: [
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
+    RolModule,
+    UserModule,
+    LodgingModule,
+    GastronomicPlaceModule,
+    HealthModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
