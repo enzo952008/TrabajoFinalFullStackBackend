b8931edd388ace6ce62e4babe9bbf2e9f0513347
julietazotes <julietaazotes@gmail.com>
2024-11-11 19:58:40 -0300

    Agregado de nuevos decoradores de Swagger para hacer más completa la documentación

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { HealthService } from './health.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('health')
 @Controller('health')
 export class HealthController {
     constructor(private readonly healthService: HealthService) { }
+    
     @Get()
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Verificar el estado de la aplicación' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'Estado exitoso' })
     healthCheck(): string {
-        return this.healthService.getHealth()
+      return this.healthService.getHealth();
     }
 }
--- src/rol/rol.controller.ts
+++ src/rol/rol.controller.ts
@@ -3,23 +3,35 @@ import { RolService } from "./rol.service";
 import { Get } from "@nestjs/common";
 import { CreateRolDto } from "./dto/create.rol.dto";
 import { UpdateRolDto } from "./dto/update.rol.dto";
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from "@nestjs/swagger";
 
-
+@ApiTags('Roles')
 @Controller('/roles')
 export class RolController {
     constructor(private readonly rolService: RolService) { }
 
     @Get()
+    @ApiOperation({ summary: 'Obtener todos los roles' })
+    @ApiResponse({ status: 200, description: 'Roles obtenidos exitosamente' })
+    @ApiResponse({ status: 404, description: 'No se encontraron roles en la base de datos' })
     getAll() {
         return this.rolService.findAll();
     }
 
     @Post()
+    @ApiOperation({ summary: 'Crear un nuevo rol' })
+    @ApiResponse({ status: 201, description: 'Rol creado exitosamente' })
+    @ApiResponse({ status: 400, description: 'Datos de solicitud inválidos' })
+    @ApiBody({ type: CreateRolDto })
     createRol(@Body() createRolDto: CreateRolDto) {
         return this.rolService.createRol(createRolDto)
     }
 
     @Patch('/:id')
+    @ApiOperation({ summary: 'Actualizar un rol existente' })
+    @ApiParam({ name: 'id', description: 'ID del rol a actualizar' })
+    @ApiResponse({ status: 200, description: 'Rol actualizado exitosamente' })
+    @ApiResponse({ status: 404, description: 'Rol no encontrado' })
     updateRol(
         @Param('id') id: string,
         @Body() updateRolDto: UpdateRolDto
@@ -28,6 +40,10 @@ export class RolController {
     }
 
     @Delete('/:id')
+    @ApiOperation({ summary: 'Eliminar un rol por ID' })
+    @ApiParam({ name: 'id', description: 'ID del rol a eliminar' })
+    @ApiResponse({ status: 204, description: 'Rol eliminado exitosamente' })
+    @ApiResponse({ status: 404, description: 'Rol no encontrado' })
     deleteRol(
         @Param('id') id: string,
     ) {
